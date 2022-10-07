6d1d7e900de0b4619d69ab45c4dbec904e6438d5
SergioCespedes <sergicd23@gmail.com>
2022-10-06 18:33:33 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <myCalculator />
+  </div>
+</template>
+
+<script>
+import myCalculator from './components/my-calculator.vue'
+export default {
+  name: 'app',
+  components: {
+    myCalculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
